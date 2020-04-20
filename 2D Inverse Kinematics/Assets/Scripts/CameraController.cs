9477c9581bcbab3f947623c37a40be60b6552daa
jerko12 <61011416+jerko12@users.noreply.github.com>
2020-04-20 19:53:27 +0200

    Revert "Revert "2D Kinematics""
    This reverts commit f81c33f0d0f88d0edc4bb2c7cabfb93dbd1e4925.

---
+++ 2D Inverse Kinematics/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Slerp(transform.position, target.transform.position + (Vector3.up * 3) + Vector3.back, speed * Time.fixedDeltaTime) ;
+    }
+}
